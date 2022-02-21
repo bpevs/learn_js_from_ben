17a61667b79e4cdc7278325a2f94acb57d3740fb
ivebencrazy <github@bpev.me>
2022-02-20 20:46:49 -0800

    remove unnecessary module

--- lessons/The Modern Website/exercise/source/index.js
+++
@@ -1,8 +0,0 @@
-import { randomColor } from "./utilities/randomColor";
-
-// Whenever we click `.my-title`, change the color
-const myTitle = document.getElementById("my-title");
-
-myTitle.addEventListener("click", function (event) {
-  event.target.style.color = randomColor();
-});
